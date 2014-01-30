c68d236eb7768fedddaca40b8f80a859d47a10ce
llafuente <llafuente@noboxout.com>
2014-01-30 13:04:20 +0100

    test for constraints and fixes

--- test/test.js
+++ test/test.js
@@ -112,11 +112,11 @@ process.exit();
     });
 
 
-    test("print is table", function (t) {
-        console.log(utilitario.is);
-        console.log(table.toString());
-        t.end();
-    });
+    //test("print is table", function (t) {
+    //    console.log(utilitario.is);
+    //    console.log(table.toString());
+    //    t.end();
+    //});
 
 
     test("cast", function (t) {
@@ -192,15 +192,193 @@ process.exit();
         });
 
         t.end();
-
     });
 
+    // test("print cast table", function (t) {
+    //     console.log(utilitario.cast);
+    //     console.log(table.toString());
+    //     t.end();
+    // });
 
-    test("print cast table", function (t) {
-        console.log(utilitario.cast);
-        console.log(table.toString());
+    test("constraints", function(t) {
+        var constraints = utilitario.constraints;
+
+        t.throws(function() {
+            constraints.in("x", {});
+        });
+
+        t.throws(function() {
+            constraints.in("x", "y");
+        });
+
+        t.throws(function() {
+            constraints.in();
+        });
+
+
+        t.equal(constraints.in("x", ["x"]), true, "x in [x]");
+        t.equal(constraints.in("x", ["y"]), false, "x in [y]");
+
+        t.equal(constraints.notIn("x", ["x"]), false, "x notIn [x]");
+        t.equal(constraints.notIn("x", ["y"]), true, "x notIn [y]");
+
+        t.throws(function() {
+            constraints.contains("x", {});
+        });
+
+        t.throws(function() {
+            constraints.contains("x", []);
+        });
+
+        t.throws(function() {
+            constraints.contains();
+        });
+
+        t.equal(constraints.contains("x", "abcxyz"), true, "abcxyz contains x");
+        t.equal(constraints.contains("h", "abcxyz"), false, "abcxyz contains h");
+
+        t.equal(constraints.notContains("x", "abcxyz"), false, "abcxyz contains h");
+        t.equal(constraints.notContains("h", "abcxyz"), true, "abcxyz contains h");
+
+
+        t.equal(constraints.equals("h", "h"), true, "equal");
+        t.equal(constraints.equals("h", "b"), false, "equal");
+        t.equal(constraints.equals("0", 0), true, "equal coercion");
+        t.equal(constraints.equals(undefined, null), true, "equal coercion");
+
+        t.equal(constraints.notEquals(1, 2), true, "notEquals");
+
+
+        t.equal(constraints.equalsStrict("h", "h"), true, "equal");
+        t.equal(constraints.equalsStrict("h", "b"), false, "equal");
+        t.equal(constraints.equalsStrict("0", 0), false, "equal no coercion");
+        t.equal(constraints.equalsStrict(undefined, null), false, "equal no coercion");
+
+        t.equal(constraints.notEqualsStrict(2, 1), true, "notEqualsStrict");
+
+
+        t.equal(constraints.regex("abc", /^abc$/), true, "regex");
+        t.equal(constraints.regex("xxx", /^abc$/), false, "regex");
+
+        t.equal(constraints.notRegex("abc", /^abc$/), false, "regex");
+
+        t.throws(function() {
+            t.equal(constraints.regex("xxx"), false, "regex");
+        });
+
+
+        t.equal(constraints.dateAfter("2012-01-01", new Date("2013-01-01")), false, "dateAfter");
+        t.equal(constraints.dateAfter("2015-01-01", new Date("2013-01-01")), true, "dateAfter");
+
+        t.equal(constraints.dateBefore("2012-01-01", new Date("2013-01-01")), true, "dateBefore");
+        t.equal(constraints.dateBefore("2015-01-01", new Date("2013-01-01")), false, "dateBefore");
+
+
+        t.throws(function() {
+            t.equal(constraints.dateAfter("xxx", null), false, "regex");
+        });
+
+        t.throws(function() {
+            t.equal(constraints.dateBefore("xxx", null), false, "regex");
+        });
+
+
+        t.equal(constraints.email("@"), false, "email");
+        t.equal(constraints.email("@.com"), false, "email");
+        t.equal(constraints.email("@xx.com"), false, "email");
+        t.equal(constraints.email("abc@xx"), false, "email");
+        t.equal(constraints.email("abc@.com"), false, "email");
+        t.equal(constraints.email("abc@xx.com"), true, "email");
+
+        t.equal(constraints.email(101), false, "email");
+
+
+        t.equal(constraints.url("http://www.xxxx.com/xyz"), true, "url");
+        t.equal(constraints.url("ftp://www.xxxx.com/xyz"), true, "url");
+        t.equal(constraints.url("domain.com/xyz"), true, "url");
+
+        t.equal(constraints.url("s3://www.xxxx.com/xyz"), false, "url");
+        t.equal(constraints.url("http:/com/xyz"), false, "url");
+        t.equal(constraints.url("http//com/xyz"), false, "url");
+        t.equal(constraints.url("http://xxxx.ca?ssdfjs=Snjs"), false, "url no qs");
+
+        t.equal(constraints.url(101), false, "email");
+
+
+        t.equal(constraints.alpha("abscdfkls"), true, "alpha");
+        t.equal(constraints.alpha("á"), false, "alpha");
+        t.equal(constraints.alpha("ä"), false, "alpha");
+        t.equal(constraints.alpha("123"), false, "alpha");
+        t.equal(constraints.alpha("_-"), false, "alpha");
+        t.equal(constraints.alpha("abc-bca"), false, "alpha");
+
+        t.equal(constraints.numeric("123"), true, "numeric");
+        t.equal(constraints.numeric(123), true, "numeric");
+
+        t.equal(constraints.alphanumeric("abc123bca"), true, "alpha");
+        t.equal(constraints.alphanumeric("."), false, "alpha");
+
+        t.equal(constraints.hexadecimal("abc"), true, "hexadecimal");
+        t.equal(constraints.hexadecimal("0F"), true, "hexadecimal");
+        t.equal(constraints.hexadecimal("0f"), true, "hexadecimal");
+
+        t.equal(constraints.hexadecimal("jhg"), false, "hexadecimal");
+
+
+        t.equal(constraints.length("", 1, 10), false, "length");
+        t.equal(constraints.length("abc", 1, 10), true, "length");
+        t.equal(constraints.length("abasd4sa5dfdsfs6a516dsc", 1, 10), false, "length");
+
+        t.equal(constraints.length([], 1, 10), false, "length");
+        t.equal(constraints.length(["x"], 1, 10), true, "length");
+        t.equal(constraints.length([,,,,,,,,,,,,,,,,"x"], 1, 10), false, "length");
+
+        t.throws(function() {
+            t.equal(constraints.length([,,,,,,,,,,,,,,,,"x"], null), false, "length");
+        });
+
+
+        t.equal(constraints.min("xXXXx", 5), true, "x5 min str");
+        t.equal(constraints.min(15, 5), true, "min number");
+
+        t.equal(constraints.min("xxx", 5), false, "x3 min str");
+        t.equal(constraints.min(4, 5), false, "min number");
+
+        t.equal(constraints.max("xxx", 5), true, "x3 max str");
+        t.equal(constraints.max(4, 5), true, "max number");
+
+        t.equal(constraints.max("xxXXXx", 5), false, "x5 max str");
+        t.equal(constraints.max(15, 5), false, "max number");
+
+        t.throws(function() {
+            t.equal(constraints.max("xxXXXx", null), false, "length");
+        });
+
+        t.throws(function() {
+            t.equal(constraints.min("xxXXXx", null), false, "length");
+        });
+
+
+        [{}, undefined, [], NaN, null].forEach(function(val) {
+            t.equal(constraints.alphanumeric(val), false, "alphanumeric");
+            t.equal(constraints.alpha(val), false, "alpha");
+            t.equal(constraints.numeric(val), false, "numeric");
+            t.equal(constraints.hexadecimal(val), false, "hexadecimal");
+
+            t.equal(constraints.min(val, -1), false, "min");
+            t.equal(constraints.max(val, -1), false, "max");
+
+            if (!Array.isArray(val)) {
+                t.equal(constraints.length(val, 0, 10), false, "length");
+            }
+
+        });
+
+
         t.end();
-    });
+    })
+
+
 
 
 
